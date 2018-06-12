3acf04a3bf86e8c5f371efb0ae146d451720ca8f
jmz <1125378902@qq.com>
2018-06-12 15:16:50 +0800

    首页文章列表页完成

--- resources/views/default/article.blade.php
+++ resources/views/default/article.blade.php
@@ -5,19 +5,23 @@ $author = isset($user->id) ? $user : $userPresenter->getUserInfo();
 @if($articles[0]!="")
     @foreach($articles as $article)
         <div class="blogs" data-scroll-reveal="enter bottom over 1s" >
-            <span class="blogpic"><a href="{{ route('article',['id'=>$article->id]) }}" title=""><img src="{{ $article->list_pic }}" alt=""></a></span>
-            <h3 class="blogtitle"><a href="{{ route('article',['id'=>$article->id]) }}">{{ $article->title }}</a></h3>
-            <p class="blogtext">{{ $article->desc }} </p>
-            <div class="bloginfo">
-                <ul>
-                    <li class="author"><a>{{ $author->name }}</a></li>
-                    @if($article->category)
-                    <li class="lmname"><a href="{{ route('category', ['id' => $article->cate_id]) }}">{{ $article->category->name }}</a></li>
-                    @endif
-                    <li class="timer">{{ date('Y-m-d',strtotime($article->created_at)) }}</li>
-                    <li class="view"><span>{{ $article->read_count }}</span> </li>
-                    <li class="comment">{{ $article->comment_count }}</li>
-                </ul>
+            <div class="blogs_left">
+                <span class="blogpic"><a href="{{ route('article',['id'=>$article->id]) }}" title=""><img src="{{ $article->list_pic }}" alt=""></a></span>    
+            </div>
+            <div class="blogs_right">
+                <h3 class="blogtitle"><a href="{{ route('article',['id'=>$article->id]) }}">{{ $article->title }}</a></h3>
+                <p class="blogtext">{{ $article->desc }} </p>
+                <div class="bloginfo">
+                    <ul>
+                        <li class="author"><a>{{ $author->name }}</a></li>
+                        @if($article->category)
+                        <li class="lmname"><a href="{{ route('category', ['id' => $article->cate_id]) }}">{{ $article->category->name }}</a></li>
+                        @endif
+                        <li class="timer">{{ date('Y-m-d',strtotime($article->created_at)) }}</li>
+                        <li class="view"><span>{{ $article->read_count }}</span> </li>
+                        <li class="comment">{{ $article->comment_count }}</li>
+                    </ul>
+                </div>
             </div>
         </div>
     @endforeach
